b27dbcb0003518750facf3abd15b5b0804c271eb
Leandro3242 <127863710+Leandro3242@users.noreply.github.com>
2023-10-04 23:22:00 -0300

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ import moment from 'moment-timezone';
 global.owner = [
   ['5492976232784', '👑 Mystic - Creador 👑', true],
   ['573208266162', '👑 Mystic - Creador 👑', true],
+  ['51921139017', '👑 Mystic - Creador 👑', true],
 
 ];
 
